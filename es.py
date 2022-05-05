fee2be7266046ae273da8bdb8b551b28f46ada25
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2022-05-05 09:54:30 +0000

    ok

--- es.py
+++ es.py
@@ -19,23 +19,67 @@ def home():
 
   return render_template("home.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
 
-@app.route("/selsquadra", methods=["GET"])
+@app.route("/selruolo", methods=["GET"])
 def selsquadra():
   #radio button
   sceltaruolo= request.args["scelta"]
   if sceltaruolo == "AllRoles":
-    listaGioc= lstGioc
+    listaGioc = lstGioc
+    return render_template("allroles.html", listaGioc = listaGioc.to_html(border=0))
   elif sceltaruolo == "P":
-    listaGioc= lstPort
+    listaGioc = lstPort
+    return render_template("portieri.html", listaGioc = listaGioc.to_html(border=0))
   elif sceltaruolo == "D":
-    listaGioc= lstDif
+    listaGioc = lstDif
+    return render_template("difensori.html", listaGioc = listaGioc.to_html(border=0))
   elif sceltaruolo == "C":
-    listaGioc= lstCen
+    listaGioc = lstcen
+    return render_template("centrocampisti.html", listaGioc = listaGioc.to_html(border=0))
   elif sceltaruolo == "A":
-    listaGioc= lstAtt
-
-
+    listaGioc = lstAtt
+    return render_template("attaccanti.html", listaGioc = listaGioc.to_html(border=0))
+   
+  
+  
   return render_template("home.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+@app.route("/allroles", methods=["GET"])
+def allroles():
+    elSquadre = lstGioc['Squadra'].drop_duplicates().sort_values(ascending=True)
+    listaGioc = lstGioc
+    return render_template("allroles.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+@app.route("/por", methods=["GET"])
+def por():
+    elSquadre = lstGioc['Squadra'].drop_duplicates().sort_values(ascending=True)
+    listaGioc = lstPort
+    return render_template("por.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+@app.route("/dif", methods=["GET"])
+def dif():
+    elSquadre = lstGioc['Squadra'].drop_duplicates().sort_values(ascending=True)
+    listaGioc = lstDif
+    return render_template("por.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+@app.route("/cen", methods=["GET"])
+def cen():
+    elSquadre = lstGioc['Squadra'].drop_duplicates().sort_values(ascending=True)
+    listaGioc = lstCen
+    return render_template("por.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+@app.route("/att", methods=["GET"])
+def att():
+    elSquadre = lstGioc['Squadra'].drop_duplicates().sort_values(ascending=True)
+    listaGioc = lstAtt
+    return render_template("por.html", listaGioc = listaGioc.to_html(border=0), squadre= elSquadre)
+
+
+
 
 
 if __name__ == '__main__':
